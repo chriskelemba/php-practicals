9ba21915d3000d674b7374109e007b49a1351666
chriskelemba <loverhxchris@gmail.com>
2024-05-14 22:52:52 +0300

    sign up redirects to login

--- process_login.php
+++ process_login.php
@@ -5,9 +5,9 @@ $username = trim($_POST['username']);
 $email = trim($_POST['email']);
 $password = trim($_POST['password']);
 
-$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND email = ? AND password = ?");
-$stmt->bind_param("sss", $username, $email, $password);
-$stmt->execute();
+$stmt = $conn -> prepare("SELECT * FROM users WHERE username = ? AND email = ? AND password = ?");
+$stmt -> bind_param("sss", $username, $email, $password);
+$stmt -> execute();
 $result = $stmt->get_result();
 
 if ($result -> num_rows > 0) {
--- process_signup.php
+++ process_signup.php
@@ -8,7 +8,7 @@ $password = $_POST['password'];
 $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "User signed up.";
+    header('Location: login.php');
 } else {
     echo "An error has occured: " .mysqli_error($conn);
 }
