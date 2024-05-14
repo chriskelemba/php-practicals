392b197e6c679c2d27467bd7ffdae9feb66d0f71
chriskelemba <loverhxchris@gmail.com>
2024-05-14 22:18:34 +0300

    sign up styling added

--- signup.php
+++ signup.php
@@ -3,18 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" integrity="sha256-2TnSHycBDAm2wpZmgdi0z81kykGPJAkiUY+Wf97RbvY=" crossorigin="anonymous">
     <title>Signup</title>
 </head>
 <body>
     <!-- Signup Form -->
-    <form action="process_signup.php" method="post">
-        <label>Username:</label></br>
-        <input type="text" name="username" placeholder="Enter your username" required/></br></br>
-        <label>Email:</label></br>
-        <input type="email" name="email" placeholder="Enter your email" required/></br></br>
-        <label>Password:</label></br>
-        <input type="password" name="password" placeholder="Enter your password" required/></br></br>
-        <input type="submit" name="submit" value="Sign Up"/>
+    <form action="process_signup.php" method="post" class="p-5 w-50 mx-auto">
+        <legend>Sign Up</legend>
+        <div class="form-group">
+            <label>Username:</label></br>
+            <input type="text" name="username" class="form-control" placeholder="Enter your username" required/></br></br>
+        </div>
+        <div class="form-group">
+            <label>Email:</label></br>
+            <input type="email" name="email" class="form-control" placeholder="Enter your email" required/></br></br>
+        </div>
+        <div class="form-group">
+            <label>Password:</label></br>
+            <input type="password" name="password" class="form-control" placeholder="Enter your password" required/></br></br>
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary" value="Sign Up">Sign Up</button>
     </form>
 </body>
 </html>
