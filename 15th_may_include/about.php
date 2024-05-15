e48df1c0a1aca58fef8b411cb49e6a328144046e
chriskelemba <loverhxchris@gmail.com>
2024-05-15 15:42:05 +0300

    15th may progress

--- 15th_may_include/about.php
+++ 15th_may_include/about.php
@@ -3,21 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"/>
     <title>About Us</title>
 </head>
 <body>
     <!-- Navigation Bar -->
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="service.php">Service</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="signup.php">Signup</a></li>
-        </ul>
-    </nav>
+    <?php
+    include("navbar.php")
+    ?>
     <div>
         <h1>About Us</h1>
     </div>
---
+++ 15th_may_include/navbar.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="style.css"/>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="service.php">Service</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="../login.php">Login</a></li>
+        <li><a href="signup.php">Signup</a></li>
+    </ul>
+</nav>
