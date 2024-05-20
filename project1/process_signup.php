4286d54d8a0db732af97b0dd52236e7af78eb326
chriskelemba <loverhxchris@gmail.com>
2024-05-20 10:51:14 +0300

    added email validation

--- project1/process_signup.php
+++ project1/process_signup.php
@@ -7,11 +7,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = trim($_POST["password"]);
     $confirmPassword = trim($_POST["confirmPassword"]);
 
+
+    // Checks whether email already exists in the database
+    $emailSql = "SELECT * FROM accounts WHERE email = '$email'";
+    $result = mysqli_query($conn, $emailSql);
+    if (mysqli_num_rows($result) > 0) {
+        header('Location: error_emailexists.php');
+        exit;
+    }
+
+    // Checks whether the password matches
     if ($password !== $confirmPassword) {
         header('Location: error_signup.php');
         exit;
     }
 
+    // Hashes the password
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO accounts (username, email, password, role) VALUES ('$username', '$email', '$password', 'User')";
